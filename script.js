c05720705ed313f5257309ad293cd6207f308c4a
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-22 11:45:40 -0700

    added math

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var userOption = ["Rock", "Paper", "Scissors"]
 var computerOption = ["Rock", "Paper", "Scissors"]
 var computerChosenOption = computerOption[Math.floor(Math.random() * computerOption.length)];
+var scores = ["wins", "losses", "ties"]
 
 
-
-function startButton() {
+  function startButton(computerChosenOption, userOption) {
     userOption = prompt("Rock, Paper, or Scissors?");
-    if (userOption != null) {
+      if (computerChosenOption === userOption) {
         alert(`The computer chose ${computerChosenOption} !`);
-        var results = confirm("These are the results: play again?");
-        return;
-    }
+        var results = confirm(`You both chose the same option: ${scores} play again?`);
+      } else if (computerChosenOption !== userOption) {
+        alert(`The computer chose ${computerChosenOption} !`);
+      } else {
+        var results = confirm(`The computer won, here are the scores ${scores} play again?`);
+      }
   }
+
