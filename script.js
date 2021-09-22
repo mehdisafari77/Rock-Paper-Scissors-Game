b81cc758e28e9ea93900ce30cbb5cdbd86ea4d54
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-22 15:23:40 -0700

    finalized css, javascript, and html

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-var userOption = ["Rock", "Paper", "Scissors"]
-var computerOption = ["Rock", "Paper", "Scissors"]
+var userOption = ["r", "p", "s"];
+var computerOption = ["r", "p", "s"];
 var computerChosenOption = computerOption[Math.floor(Math.random() * computerOption.length)];
-var scores = ["wins", "losses", "ties"]
+var scores = ["wins", "losses", "ties"];
 
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
-  function startButton(computerChosenOption, userOption) {
-    userOption = prompt("Rock, Paper, or Scissors?");
-      if (computerChosenOption === userOption) {
+for (var i = 0; i <10 ; i++) {
+    var computerOption = computerChosenOption;
+}
+
+function playGame() {
+    userOption = prompt("R, P, or S?");
+    userOption = userOption.toLowerCase();
+    if (userOption === "r" || userOption === "p" || userOption === "s") {
         alert(`The computer chose ${computerChosenOption} !`);
-        var results = confirm(`You both chose the same option: ${scores} play again?`);
-      } else if (computerChosenOption !== userOption) {
-        alert(`The computer chose ${computerChosenOption} !`);
-      } else {
-        var results = confirm(`The computer won, here are the scores ${scores} play again?`);
-      }
-  }
 
+        if (( userOption === "r" && computerChosenOption === "s") ||
+        (userOption === "s" && computerChosenOption === "p") ||
+        (userOption === "p" && computerChosenOption === "r")) {
+            wins++;
+            alert(`You have won ${wins} time(s)!`);
+        }  else if (userOption === computerChosenOption) {
+            ties++
+            alert(`You have tied ${ties} time(s).`);
+        } else if (userOption !== computerChosenOption){
+            losses++
+            alert(`You have lost ${losses} time(s).`);
+        } else {
+            alert('Pleas enter only R, P, or S')
+        }
+    }
+    alert(`Total wins: ${wins} \nTotal ties: ${ties} \nTotal losses: ${losses}`)
+    let ifPlayAgain = confirm("Press OK if you want to play again!")
+    if (ifPlayAgain) {
+        playGame()
+    }
+}
+
+document.getElementById('wins').innerHTML = wins
+document.getElementById('losses').innerHTML = losses
+document.getElementById('ties').innerHTML = ties
