4370d0f0375a011db804e1ef6374bd0fca9b639d
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-22 11:27:34 -0700

    add prompt() alert()

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Rock-Paper-Scissors-Game
+### Specifications
+
+* Must use the `alert()`, `confirm()`, and `prompt()` methods to collect user input and display information to the user.
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+var userOption = ["Rock", "Paper", "Scissors"]
+var computerOption = ["Rock", "Paper", "Scissors"]
+var computerChosenOption = computerOption[Math.floor(Math.random() * computerOption.length)];
+var results = confirm("These are the results: play again?");
+
 
 function startButton() {
-    var chosenOption = prompt("Rock, Paper, or Scissors?");
-    if (chosenOption != null) {
-      document.getElementById("RPS").innerHTML =
-      "You chose " + chosenOption + "but the computer chose X?";
+    userOption = prompt("Rock, Paper, or Scissors?");
+    if (userOption != null) {
+        alert(`The computer chose ${computerChosenOption} !`);
+        return;
+
     }
   }
