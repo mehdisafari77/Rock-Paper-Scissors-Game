d8bd529644ed12eee0f562381b89716b621d92fc
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-22 11:29:03 -0700

    fixed confirm() situation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var userOption = ["Rock", "Paper", "Scissors"]
 var computerOption = ["Rock", "Paper", "Scissors"]
 var computerChosenOption = computerOption[Math.floor(Math.random() * computerOption.length)];
-var results = confirm("These are the results: play again?");
+
 
 
 function startButton() {
     userOption = prompt("Rock, Paper, or Scissors?");
     if (userOption != null) {
         alert(`The computer chose ${computerChosenOption} !`);
+        var results = confirm("These are the results: play again?");
         return;
-
     }
   }
